a82b5ce310bc2ff8775bf4b1e0a80857efda05ca
iamdipankarj <iamdipankarj@gmail.com>
2024-03-07 20:57:31 +0530

    Add all AI model pages.

---
+++ src/app/text-to-image/page.tsx
@@ -0,0 +1,9 @@
+export default function TextToImagePage() {
+  return (
+    <main className="flex flex-col flex-1 items-stretch justify-center space-y-2">
+      <section className="container px-4 mx-auto">
+        TextToImage
+      </section>
+    </main>
+  )
+}
---
+++ src/app/upscale/page.tsx
@@ -0,0 +1,9 @@
+export default function UpscalePage() {
+  return (
+    <main className="flex flex-col flex-1 items-stretch justify-center space-y-2">
+      <section className="container px-4 mx-auto">
+        Upscale Images
+      </section>
+    </main>
+  )
+}
