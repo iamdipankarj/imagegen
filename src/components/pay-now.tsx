b606539b742582ad22971cbaab15d2a1fe1774a2
iamdipankarj <iamdipankarj@gmail.com>
2024-03-16 19:17:30 +0530

    Move pricing model to client side component.

--- src/components/pay-now.tsx
+++ src/components/pay-now.tsx
@@ -1,15 +1,13 @@
 import { cn } from "@/lib/utils";
 import { Banknote } from "lucide-react";
 
-export function PayNowButton({ className, href, ...restProps }: React.AnchorHTMLAttributes<HTMLAnchorElement>) {
+export function PayNowButton({
+  className, ...restProps
+}: React.HTMLAttributes<HTMLButtonElement>) {
   return (
-    <a
-      className={cn("btn btn-primary btn-block group", className)}
-      href={href}
-      {...restProps}
-    >
+    <button className={cn("btn btn-primary btn-block group", className)} {...restProps}>
       Buy Now
       <Banknote className="w-5 h-5 fill-white/10 group-hover:translate-x-0.5 group-hover:fill-white/20 transition-transform duration-200" />
-    </a>
+    </button>
   )
 }
--- src/components/pricing-component.tsx
+++ src/components/pricing-component.tsx
@@ -4,31 +4,30 @@ import { PayNowButton } from "@/components/pay-now";
 import { PriceColumn } from "@/components/price-column";
 import { PLUS_PRICE_ID, PRO_PRICE_ID, STARTER_PRICE_ID } from "@/lib/credits";
 import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 
 export function PricingComponent() {
   const { user: clerkUser } = useUser();
+  const router = useRouter();
 
   const userEmail = clerkUser?.emailAddresses?.[0]?.emailAddress;
 
-  function getStarterCheckoutLink(priceId: string): string {
+  const handleStarterLink = (e: any) => {
     if (clerkUser && userEmail) {
-      return `https://photoworksai.lemonsqueezy.com/buy/a5bc9402-5aee-4e7c-bd61-1662ed373a68?checkout[custom][clerk_user_id]=${clerkUser.id}&checkout[custom][price_id]=${priceId}&checkout[custom][user_email]=${userEmail}`
+      router.push(`https://photoworksai.lemonsqueezy.com/buy/a5bc9402-5aee-4e7c-bd61-1662ed373a68?checkout[custom][clerk_user_id]=${clerkUser.id}&checkout[custom][price_id]=${STARTER_PRICE_ID}&checkout[custom][user_email]=${userEmail}`);
     }
-    return "#"
   }
 
-  function getPlusCheckoutLink(priceId: string): string {
+  const handlePlusLink = (e: any) => {
     if (clerkUser && userEmail) {
-      return `https://photoworksai.lemonsqueezy.com/buy/70258e49-d508-4e70-8689-bbcfddffb8f7?checkout[custom][clerk_user_id]=${clerkUser.id}&checkout[custom][price_id]=${priceId}&checkout[custom][user_email]=${userEmail}`
+      router.push(`https://photoworksai.lemonsqueezy.com/buy/70258e49-d508-4e70-8689-bbcfddffb8f7?checkout[custom][clerk_user_id]=${clerkUser.id}&checkout[custom][price_id]=${PLUS_PRICE_ID}&checkout[custom][user_email]=${userEmail}`)
     }
-    return "#"
   }
 
-  function getProCheckoutLink(priceId: string): string {
+  const handleProLink = (e: any) => {
     if (clerkUser && userEmail) {
-      return `https://photoworksai.lemonsqueezy.com/buy/b4ae4c3d-1eed-44a5-8820-83d24ce27c30?checkout[custom][clerk_user_id]=${clerkUser.id}&checkout[custom][price_id]=${priceId}&checkout[custom][user_email]=${userEmail}`
+      router.push(`https://photoworksai.lemonsqueezy.com/buy/b4ae4c3d-1eed-44a5-8820-83d24ce27c30?checkout[custom][clerk_user_id]=${clerkUser.id}&checkout[custom][price_id]=${PRO_PRICE_ID}&checkout[custom][user_email]=${userEmail}`)
     }
-    return "#"
   }
 
   return (
@@ -42,7 +41,7 @@ export function PricingComponent() {
             "Every Feature Available"
           ]}
         >
-          <PayNowButton href={getStarterCheckoutLink(STARTER_PRICE_ID)} rel="nofollow" />
+          <PayNowButton onClick={handleStarterLink} />
         </PriceColumn>
         <PriceColumn
           heading="50 Credits"
@@ -52,7 +51,7 @@ export function PricingComponent() {
             "Every Feature Available"
           ]}
         >
-          <PayNowButton href={getPlusCheckoutLink(PLUS_PRICE_ID)} rel="nofollow" />
+          <PayNowButton onClick={handlePlusLink} />
         </PriceColumn>
         <PriceColumn
           heading="100 Credits"
@@ -62,7 +61,7 @@ export function PricingComponent() {
             "Every Feature Available"
           ]}
         >
-          <PayNowButton href={getProCheckoutLink(PRO_PRICE_ID)} rel="nofollow" />
+          <PayNowButton onClick={handleProLink} />
         </PriceColumn>
     </>
   )
