b7d98abbec474763f7763102abbabd233577ffe4
iamdipankarj <iamdipankarj@gmail.com>
2024-03-19 11:19:38 +0530

    Use client provider for clerk.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
 import { Gabarito as FontSans } from "next/font/google";
-import { ClerkProvider } from '@clerk/nextjs'
 import { Toaster } from 'sonner'
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { AppHeader } from "@/components/app-header";
 import { Footer } from "@/components/footer";
 import { getMetaData, getStructuredData } from "@/lib/seo";
+import SessionProvider from "@/app/session-provider";
 // import ProgressProvider from "@/components/progress-provider";
 
 declare global {
@@ -29,24 +29,24 @@ export default async function RootLayout({
 }>) {
 
   return (
-    <ClerkProvider>
-      <html lang="en" suppressHydrationWarning data-theme="light">
-        <head>
-          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" />
-          {process.env.NODE_ENV === "production" ? (
-            <>
-              <link rel="preconnect" href="https://plausible.io" crossOrigin="anonymous" />
-              <script defer data-domain="photoworks.ai" src="https://plausible.io/js/script.js" />
-            </>
-          ) : null}
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#41c289" />
-          <script
-            key="structured-data"
-            type="application/ld+json"
-            dangerouslySetInnerHTML={{ __html: JSON.stringify(getStructuredData()) }}
-          />
-        </head>
-        <body className={cn("min-h-screen font-sans flex flex-col antialiased", fontSans.variable)}>
+    <html lang="en" suppressHydrationWarning data-theme="light">
+      <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" />
+        {process.env.NODE_ENV === "production" ? (
+          <>
+            <link rel="preconnect" href="https://plausible.io" crossOrigin="anonymous" />
+            <script defer data-domain="photoworks.ai" src="https://plausible.io/js/script.js" />
+          </>
+        ) : null}
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#41c289" />
+        <script
+          key="structured-data"
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(getStructuredData()) }}
+        />
+      </head>
+      <body className={cn("min-h-screen font-sans flex flex-col antialiased", fontSans.variable)}>
+        <SessionProvider>
           <AppHeader />
           {children}
           <Footer />
@@ -55,8 +55,8 @@ export default async function RootLayout({
             position="top-center"
             richColors
           />
-        </body>
-      </html>
-    </ClerkProvider>
+        </SessionProvider>
+      </body>
+    </html>
   );
 }
