cd62b0cfb6596859c405cfef7b99527d1f8a554e
Dipankar Jana <iamdipankarj@gmail.com>
2026-06-02 17:20:57 +0530

    change tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
-import type { Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
-const config: Config = {
+const config = {
   darkMode: "class",
   daisyui: {
     themes: [{
